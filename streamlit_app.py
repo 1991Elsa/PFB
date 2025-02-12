ca5eea49070fd7ae5a6c9ac0303432e5741b3c40
Guadalupe <penaegeaguadalupe@gmail.com>
2025-02-12 13:55:29 +0100

    creado un archivo.py para maketar streamlit, un entorno virtural, y la carpeta git ignore para poder poner .env con la informacion sensible de manera que no la muestre

---
+++ streamlit_app.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+
+def main():
+        st.title("PFB Yahoo Finance")
+        st.write("Bienvenidos a la demo del PFB de Yahoo Finance")
+
+if __name__ == "__main__":  
+    main()
+
