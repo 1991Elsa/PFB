cd0fd59d24a3cac706ca123558fd94209876a776
Wantumaka <140568976+Wantumaka@users.noreply.github.com>
2025-02-12 18:10:21 +0100

    Delete modules directory

--- modules/pfb_page_config_dict.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-# Configuración de la página con los parámetros que se desean en formato de diccionario.
-
-PAGE_CONFIG = {"page_title"            : "PFB Yahoo Finance", # Título de la página
-               #"page_icon"             : "	:chart:", # Icono de la página
-               "page_icon"             : Image.open("sources/YF_icono.png"),
-               "layout"                : "wide",     
-               "initial_sidebar_state" : "collapsed"} # collapsed o expanded para que el menu este abierto o cerrado por defecto
