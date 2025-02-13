cb110f19360ce81985158d9ca7a848afa14b101c
Wantumaka <mikelalonso1991@gmail.com>
2025-02-13 17:46:10 +0100

    Actualizado requieremnts y prueba script

--- prueba_script_info.py
+++ prueba_script_info.py
@@ -19,26 +19,21 @@ def get_ticker_info (ticker):
 
     return ticker_info
 
-nasdaq_tickers_info = pd.DataFrame() 
+
+nasdaq_tickers_historic = pd.DataFrame()
+nasdaq_tickers_info = pd.DataFrame()
 
 for ticker in tickers_nasdaq:
-    #llamamos a las funciones para extraer data del ticker
     ticker_info = get_ticker_info(ticker)
-
     ticker_historic = get_datos_historicos(ticker)
-    
-    #cogemos la info individual que nos interesa
     dic_info= {'Ticker': ticker_info['symbol'],'sector': ticker_info['sector'], 'industry': ticker_info['industry'],'shares' : ticker_info['sharesOutstanding'] }
-    
     df = pd.DataFrame([dic_info])
 
-    #cambiamos de orientacion el df de datos historicos
-    dft = ticker_historic.T.reset_index()
+    #creamos df de info de todos los tickers
+    nasdaq_tickers_info = pd.concat([nasdaq_tickers_info, df], ignore_index=True)
 
-    #hacemos merge de los dos df
-    dfmerge= pd.merge(df, dft, on='Ticker') 
-
-    #guardamos en un df final concatenando toda la info
-    nasdaq_tickers_info = pd.concat([nasdaq_tickers_info, dfmerge], ignore_index=True)
+    #creamos df de todos los historicos
+    nasdaq_tickers_historic = pd.concat([nasdaq_tickers_historic, ticker_historic], ignore_index=True, axis=1)
+ 
 
 
