12e7992bb162b8388d213c6ba6bd2a967cca01ae
1991Elsa <cecibel.melara@gmail.com>
2025-02-15 22:54:47 +0100

    Archivo oculto para la configuración de engine, cambio de cursor a engine

--- cursor_mysql.py
+++ cursor_mysql.py
@@ -1,32 +1,27 @@
 import pandas as pd
-import numpy as np
+import sklearn
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from funcion_extraccion_info_historicos import df_nasdaq_tickers_historic_clean, df_nasdaq_tickers_info_clean
+from config_engine import configuracion_engine
 
-from funcion_extraccion_info_historicos import df_nasdaq_tickers_historic_clean
-from funcion_extraccion_info_historicos import df_nasdaq_tickers_info_clean
+password, username, host, port = configuracion_engine()
 
-DB_USER = "root"
-DB_PASSWORD = "Dunidu"
-DB_HOST ="localhost"
-DB_NAME = "yahoo_finance"
+try:
+    engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:{port}/")
+    with engine.connect() as connection:
+        connection.execute(text("CREATE DATABASE IF NOT EXISTS yahoo_finance"))
+        print("Base de datos creada con exito")
 
-engine = create_engine(f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}")
+    engine = create_engine(f"mysql+pymysql://{username}:{password}@{host}:{port}/yahoo_finance")
 
-def insert_data(df: pd.DataFrame, table_name: str, engine):
-    if df.empty:
-        print(f"No hay datos en el DataFrame para insertar a la tabla {table_name}.")
-        return
-    
-    try:
-        with engine.begin() as conn:
-            df.to_sql(name=table_name, con=conn, if_exists="append",index=False)
-        print(f"Anadidas {len(df)} filas en la tabla {table_name}")
-    except Exception as e:
-        print(f"Error al insertar datos en {table_name}: {e}")
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT 1"))
+        print("Conexión establecida con éxito a la base de datos")
 
-from funcion_extraccion_info_historicos import df_nasdaq_tickers_historic_clean
-from funcion_extraccion_info_historicos import df_nasdaq_tickers_info_clean
+    df_nasdaq_tickers_info_clean.to_sql(name="information", con=engine, if_exists="replace", index=False)
+    df_nasdaq_tickers_historic_clean.to_sql(name="historic", con=engine, if_exists="replace", index=False)
+    print("Tablas creadas con éxito.")
 
-insert_data(df_nasdaq_tickers_historic_clean, "historic", engine)
-insert_data(df_nasdaq_tickers_info_clean, "company_info", engine)
+except Exception as e:
+    print(f"Error al establecer la conexión: {e}")
