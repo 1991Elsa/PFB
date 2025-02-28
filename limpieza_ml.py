f06d0dbe102d329d9e3dda4aa62d5116c1059a6a
1991Elsa <cecibel.melara@gmail.com>
2025-02-28 14:19:02 +0100

    Corrección errores datetime

--- limpieza_ml.py
+++ limpieza_ml.py
@@ -20,29 +20,50 @@ nasdaq_tickers_info["FreeCashflow"] = nasdaq_tickers_info.groupby("Sector")["Fre
 # Limpieza de datos historic
 
 
-
 nans = nasdaq_tickers_historic[nasdaq_tickers_historic.isna().any(axis=1)]
 
-def buscar_linealidad_temporal_nan(df):
-    rango_fechas= pd.date_range(start= df["Date"].min(), end= df["Date"].max(), freq='MS')
-    return rango_fechas.isin(df["Date"].dt.to_period("M").dt.to_timestamp()).all()
+def verificar_linealidad_temporal(df):
+    df["Date"] = pd.to_datetime(df["Date"])
+    fechas = pd.date_range(start=df["Date"].min(), end=df["Date"].max(), freq="MS")
+    return fechas.isin(df["Date"]).all()
 
-tickers_linealidad_temporal_nan = nans.groupby("Ticker").apply(buscar_linealidad_temporal_nan)
+tickers_con_linealidad = (nans.groupby("Ticker").apply(verificar_linealidad_temporal).loc[lambda x: x].index.tolist())
 
-tickers_linealidad_temporal_nan = tickers_linealidad_temporal_nan[tickers_linealidad_temporal_nan].index.tolist()
-print("Tickers de empresas con fechas continuas en las filas con valores nulos:", tickers_linealidad_temporal_nan)
+sin_linealidad_temporal = list(set(nans["Ticker"]) - set(tickers_con_linealidad))
 
-nasdaq_tickers_historic = nasdaq_tickers_historic[~nasdaq_tickers_historic["Ticker"].isin(tickers_linealidad_temporal_nan)]
+# Eliminar los tickers con linealidad temporal
+nasdaq_tickers_historic = nasdaq_tickers_historic[~nasdaq_tickers_historic["Ticker"].isin(tickers_con_linealidad)].reset_index(drop=True)
 
-sin_linealidad_temporal = tickers_linealidad_temporal_nan[~tickers_linealidad_temporal_nan].index.tolist()
-print("Tickers de empresas con fechas no continuas en las filas con valores nulos:", sin_linealidad_temporal)
+# Interpolar datos para los tickers sin linealidad temporal
+nasdaq_tickers_historic["Date"] = pd.to_datetime(nasdaq_tickers_historic["Date"])
+nasdaq_tickers_historic = nasdaq_tickers_historic.sort_values(["Ticker", "Date"])
 
-nasdaq_tickers_historic = nasdaq_tickers_historic.sort_values(by=["Ticker", "Date"])
+for ticker in sin_linealidad_temporal:
+    ticker_sin_linealidad = nasdaq_tickers_historic["Ticker"] == ticker
+    nasdaq_tickers_historic.loc[ticker_sin_linealidad, ["Close", "High", "Low", "Open", "Volume"]] = (
+    nasdaq_tickers_historic.loc[ticker_sin_linealidad].set_index("Date")[["Close", "High", "Low", "Open", "Volume"]]
+    .interpolate(method="time", limit_direction="both").values)
 
-tickers_sin_linealidad_temporal = nans[nans["Ticker"].isin(sin_linealidad_temporal)]["Ticker"].unique()
+nasdaq_tickers_historic = nasdaq_tickers_historic.reset_index(drop=True)
 
-nasdaq_tickers_historic.loc[tickers_sin_linealidad_temporal, ["Close", "High", "Low", "Open", "Volume"]] = (nasdaq_tickers_historic.loc[tickers_sin_linealidad_temporal].set_index("Date")[["Close", "High", "Low", "Open", "Volume"]].interpolate(method="time", limit_direction="both").values)
-
-print("Valores nulos después de la interpolación:")
+print("Valores nulos después de eliminación e interpolación:")
 print(nasdaq_tickers_historic.isna().sum())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
