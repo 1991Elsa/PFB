004b1a4a957726dee269777d2bff3f37189104f0
1991Elsa <cecibel.melara@gmail.com>
2025-03-05 21:43:59 +0100

    Selecciona los últimos dos años del df, revisa si ya existe la columna antes de popular

--- clustering_dbscan copy.py
+++ clustering_dbscan copy.py
@@ -28,24 +28,80 @@ nasdaq_tickers_historic
 
 """
 
+def clustering_process(engine, nasdaq_tickers_historic):
+        
+        try:
 
-        # Seleccionar características para el clustering:
+                date = nasdaq_tickers_historic["Date"].max() - pd.DateOffset(years=2)
+                nasdaq_tickers_historic = nasdaq_tickers_historic[nasdaq_tickers_historic["Date"] >= date].copy()
 
-nasdaq_tickers_historic.drop(["Ticker", "Date"], axis=1, inplace=True)
 
-X = nasdaq_tickers_historic.values
+                tickers_dates = nasdaq_tickers_historic[["Ticker", "Date"]].copy()
+                
+                nasdaq_tickers_historic.drop(["Ticker", "Date"], axis=1, inplace=True)
 
-        # Normalizar los datos usando StandardScaler:
+                X = nasdaq_tickers_historic.values
 
-X_scaler = StandardScaler()
-X = X_scaler.fit_transform(X)
+                        # Normalizar los datos usando StandardScaler:
 
-        # Entrenar el modelo DBSCAN:
+                X_scaler = StandardScaler()
+                X = X_scaler.fit_transform(X)
 
-dbscan = DBSCAN(eps=0.20, min_samples=2*X.shape[1])
-dbscan.fit(X)
+                        # Entrenar el modelo DBSCAN:
 
-Counter(dbscan.labels_)
+                dbscan = DBSCAN(eps=0.20, min_samples=2*X.shape[1])
+                dbscan.fit(X)
+
+                print(Counter(dbscan.labels_))
+
+                nasdaq_tickers_historic["Cluster"] = dbscan.labels_
+
+
+                nasdaq_tickers_historic = pd.concat([tickers_dates, nasdaq_tickers_historic], axis=1)
+
+                # Crear el engine y conectar a la base de datos yahoo_finance
+                engine = get_engine_database()
+
+                # Verificar la conexión
+                try:
+                        connection = engine.connect()
+                        connection.close()
+                        print("Conexión establecida con éxito a la base de datos yahoo_finance.")
+                except Exception as e:
+                        print(f"Error al establecer la conexión: {e}")
+
+
+                # Añadir la columna 'Cluster' a la estructura de la tabla si no existe:
+
+                with engine.connect() as conn:
+                        check_cluster = text(""" 
+                                SELECT COLUMN_NAME
+                                FROM INFORMATION_SCHEMA.COLUMNS
+                                WHERE TABLE_NAME = 'nasdaq_tickers_historic_sql' AND COLUMN_NAME = 'Cluster';
+                                """)
+                        result = conn.execute(check_cluster).fetchone()
+                        if not result:
+                                conn.execute(text("""
+                                ALTER TABLE nasdaq_tickers_historic_sql ADD COLUMN Cluster INT;
+                                """  ))
+                
+                # Insertar/actualizar los valores en la base de datos
+                with engine.begin() as conn:
+                        for _, row in nasdaq_tickers_historic.iterrows():
+                                stmt = text("""
+                                UPDATE nasdaq_tickers_historic_sql 
+                                SET Cluster = :cluster
+                                WHERE Ticker = :ticker AND Date = :date
+                                """)
+                                conn.execute(stmt, {"cluster": int(row['Cluster']), "ticker": row['Ticker'], "date": row['Date']})
+                        
+                print("Clustering actualizado en la base de datos correctamente.")
+        
+
+        except Exception as e:
+                print(f"Error en el proceso de clustering: {e}")
+
+nasdaq_tickers_historic = clustering_process(engine, nasdaq_tickers_historic)
 
 
 
