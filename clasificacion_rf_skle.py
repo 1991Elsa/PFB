d573d0ea6ac869b1aebfa17ec79674ded41518d3
1991Elsa <cecibel.melara@gmail.com>
2025-03-05 23:17:55 +0100

    Modelo de clasificación probado con cluster

--- clasificacion_rf_skle.py
+++ clasificacion_rf_skle.py
@@ -6,9 +6,22 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
-from encoding import df_info_encoded, industry_mapping
+from tratamiento_nans_clasificacion import nasdaq_tickers_historic
+
 
 def modelo_clasification(df, target_colum):
+    """
+    Filtra el dataframe nasdaq_tickers_historic para quedarse con los datos de los últimos dos años y poder correr el modelo de clasificación.
+    Usa como target la columna "Cluster" que se obtiene del modelo de clustering de los últimos dos años.
+    Normaliza los datos usando StandardScaler y entrena el modelo RandomForestClassifier.
+
+    Parámetro: Dataframe limpio con información historica de los tickers y resultados del modelo de clustering.
+
+    Retorna: Modelo de clasificación y escalador de datos con un accuracy de 0.9998
+    """
+
+    print(df.info())
+    print(df.isna().sum())
     
     X = df.drop(columns=[target_colum])
     y = df[target_colum]
@@ -34,7 +47,4 @@ def modelo_clasification(df, target_colum):
 
     return rf_model, scaler
 
-#rf_model, scaler = modelo_clasificacion(nasdaq_tickers_historic, "cluster")
-rf_model, scaler = modelo_clasification(df_info_encoded, "SectorEnergy")
-
-print(rf_model)
+rf_model, scaler = modelo_clasification(nasdaq_tickers_historic, "Cluster")
