db92e5306c5b053a58c7a7e970965e3790db1688
Guadalupe <penaegeaguadalupe@gmail.com>
2025-03-06 18:46:24 +0100

    cambio de extraccion a csv

---
+++ streamlit_modules/about_us.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+
+def mostrar(nasdaq_tickers_historic, nasdaq_tickers_info):
+    st.title("About Us")
+
+    st.write("\n")
+    st.write("Aplicación desarrollada por estudiantes de Hackaboss en el bootcamp de Data Analytics.")
+    st.write("\n")
+
+    st.header("Tecnologías Utilizadas:")
+
+    st.write("\n")
+    st.write("Python(Pandas, Numpy, Plotly, Scikit-learn, Pyspark)")
+    st.write("MySQL Workbench")
+    st.write("Streamlit")
+    st.write("Power BI")
+
+
+    st.header("Proceso de Desarrollo:")
+        
+    st.write("\n")
+    st.write("1. Extracción y tratamiento de datos:")
+    st.write("2. Creación de base de datos:")
+    st.write("3. Modelado de datos MySQL.")
+    st.write("4. Análisis de datos y visualización.")
+    st.write("5. Creación de aplicación interactiva.")
+    st.write("6. Desarrollo de algotirmos de Clustering y Clasificación.")
+    st.write("7. Integración de Power BI para vista usuario.")
+    st.write("\n")
+        
+    st.header("Equipo de Desarrollo:")
+
+    st.write("\n")
+    st.write("\n")
+
