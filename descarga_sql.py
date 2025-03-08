b9ec4f5998095b6b38cd2faf12f779683e981bec
1991Elsa <cecibel.melara@gmail.com>
2025-03-08 10:53:57 +0100

    Incluir la creación de csv para pbi

--- descarga_sql.py
+++ descarga_sql.py
@@ -43,6 +43,14 @@ def descargar_data_sql():
 
     return df_historic, df_info, df_timestamp
 
+nasdaq_tickers_historic, nasdaq_tickers_info, timestamp = descargar_data_sql()
+
+
+
+# Generamos los 3 df en formato CSV para powerBI
+nasdaq_tickers_historic.to_csv("nasdaq_tickers_historic_clean.csv", index=False)
+nasdaq_tickers_info.to_csv("nasdaq_tickers_info_clean.csv", index=False)
+timestamp.to_csv("timestamp_data_clean.csv", index=False)
 
 """
 En lugar de usar alchemy text y hacer las queries para descargar los datos de SQL como vamos a importar 
