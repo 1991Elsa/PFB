83a4219ac4178474d6d2c26f6e6fc2462740573c
1991Elsa <cecibel.melara@gmail.com>
2025-03-08 10:53:17 +0100

    Pasar la creación de csv al archivo de descarga para solucionar error error

--- obtencion_data_y_bbdd.py
+++ obtencion_data_y_bbdd.py
@@ -8,7 +8,7 @@ import time
 from connect_engine import *
 from tablas_metadata_5 import *
 from sqlalchemy.dialects.mysql import insert
-from descarga_sql import descargar_data_sql
+#from descarga_sql import descargar_data_sql
 #from clustering_dbscan import clustering_process
 
 # Función para obtener los tickers de NASDAQ 100 (scrapping)
@@ -428,10 +428,10 @@ except Exception as e:
 #except Exception as e:
     #print(f'Error al realizar el clustering: {e}')
 
-nasdaq_tickers_historic, nasdaq_tickers_info, timestamp = descargar_data_sql()
+#nasdaq_tickers_historic, nasdaq_tickers_info, timestamp = descargar_data_sql()
 
 
 # Generamos los 3 df en formato CSV para powerBI
-nasdaq_tickers_historic.to_csv("nasdaq_tickers_historic_clean.csv", index=False)
-nasdaq_tickers_info.to_csv("nasdaq_tickers_info_clean.csv", index=False)
-timestamp.to_csv("timestamp_data_clean.csv", index=False)
+#nasdaq_tickers_historic.to_csv("nasdaq_tickers_historic_clean.csv", index=False)
+#nasdaq_tickers_info.to_csv("nasdaq_tickers_info_clean.csv", index=False)
+#timestamp.to_csv("timestamp_data_clean.csv", index=False)
