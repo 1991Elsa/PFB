b77b515c34ec8d5d400bb03ab0818cbf0f3fa31c
Wantumaka <mikelalonso1991@gmail.com>
2025-03-10 18:10:56 +0100

    actualizacion pipeline obtencion data, arreglado app_sprint 2, subido pbi

--- streamlit_modules/powerbi.py
+++ streamlit_modules/powerbi.py
@@ -2,12 +2,7 @@ import streamlit as st
 
 def mostrar(nasdaq_tickers_historic, nasdaq_tickers_info):
     st.title("Dashboard Power BI")
-    import streamlit as st
-
-    
-
-    # URL del informe embebido (reemplázala si tienes el enlace de compartir)
-    power_bi_url = "https://app.powerbi.com/view?r=12771cb3-7c0d-42f9-89df-5f8ac84a9104"
-
-    # Insertar el iframe en Streamlit
-    st.components.v1.iframe(power_bi_url, width=900, height=800)
+    st.write("\n")
+    powerbi_width = 800
+    powerbi_height = 500
+    st.markdown(body = f'<iframe title="dashboard PBI" width="{powerbi_width}" height="{powerbi_height}" src="https://app.powerbi.com/view?r=eyJrIjoiN2ZhYTI2ZTAtMDU2Ni00NDc0LWFlMjAtNWY4ZmU4ZmFjMjBmIiwidCI6IjVlNzNkZTM1LWU4MjUtNGVkNS1iZTIyLTg4NTYzNTI3MDkxZSIsImMiOjl9&pageName=3bc6606a3e224c56187f" frameborder="0" allowFullScreen="true"></iframe>', unsafe_allow_html=True)
