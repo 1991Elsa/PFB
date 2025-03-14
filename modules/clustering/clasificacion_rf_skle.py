3e940cd1297e3855a6651d8441b69a2db21e23bf
1991Elsa <cecibel.melara@gmail.com>
2025-03-14 02:19:12 +0100

    Actualizar descripción de la función e incluir zero_division para quitar warnning

--- modules/clustering/clasificacion_rf_skle.py
+++ modules/clustering/clasificacion_rf_skle.py
@@ -6,27 +6,26 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
-from modules.clustering.tratamiento_nans_clasificacion import nasdaq_tickers_historic
 from sklearn.metrics import confusion_matrix
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.figure_factory as ff
 from modules.clustering.clustering_dbscan import clustering_process
 from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score
 
 def modelo_clasification(df, target_colum):
     """
-    Filtra el dataframe nasdaq_tickers_historic para quedarse con los datos de los últimos dos años y poder correr el modelo de clasificación.
-    Usa como target la columna "Cluster" que se obtiene del modelo de clustering de los últimos dos años.
-    Normaliza los datos usando StandardScaler y entrena el modelo RandomForestClassifier.
+    Crea un modelo de clasificación que normaliza los datos usando StandardScaler y entrena el modelo RandomForestClassifier.
+    Usa como target la columna "Cluster".
+    Crea el modelo de clasificación y escalador de datos en formato pkl
 
     Parámetro: Dataframe limpio con información historica de los tickers y resultados del modelo de clustering.
 
-    Retorna: Modelo de clasificación y escalador de datos con un accuracy de 0.9998
+    Retorna: modelo de clasificación y escalador de datos.
     """
 
-    print(df.info())
-    print(df.isna().sum())
+    df.drop(columns=["Date", "Ticker", "Volume"], axis=1,  inplace=True)
     
     X = df.drop(columns=[target_colum])
     y = df[target_colum]
@@ -44,10 +43,10 @@ def modelo_clasification(df, target_colum):
 
     y_pred = rf_model.predict(X_test)
 
-    print(classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred, zero_division=0))
 
     accuracy = accuracy_score(y_test, y_pred)
-    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Accuracy: {accuracy:.4f}. Modelo Clasificación")
 
     with open('modelo_clasification.pkl', 'wb') as file:
         pickle.dump(rf_model, file)
@@ -89,5 +88,4 @@ def modelo_clasification(df, target_colum):
 
     return rf_model, scaler
 
-#rf_model, scaler = modelo_clasification(nasdaq_tickers_historic, "Cluster")
 
