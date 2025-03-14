1e76be6ed5dcbc8af38ad2b22d421ddafc90bd24
Guadalupe <penaegeaguadalupe@gmail.com>
2025-03-14 11:52:16 +0100

    archivos imagenes adjuntas necesarias para cambios

--- streamlit_modules/about_us.py
+++ streamlit_modules/about_us.py
@@ -16,24 +16,26 @@ def mostrar(nasdaq_tickers_historic, nasdaq_tickers_info):
     st.write("\n")
 
     col1, col2, col3, col4 = st.columns(4)
+    
     with col1:
-        st.markdown("### **Python**")
-        st.write("Pandas") 
-        st.write("Numpy") 
-        st.write("Plotly") 
-        st.write("Scikit-learn")
-
+        st.markdown("### Desarrollo")
+        st.image("sources/I_Python.png") 
+        st.markdown("**Python**")
+        
     with col2:
-        st.markdown("### **Bases de Datos**")
-        st.write("**MySQL Workbench**")
+        st.markdown("### Bases de Datos")
+        st.image("sources/I_MySQL.png")
+        st.markdown("**MySQL Workbench**")
     
     with col3:
-        st.markdown("### **Desarrollo Web**")    
-        st.write("**Streamlit**")
+        st.markdown("### Desarrollo Web")
+        st.image("sources/I_Streamlit.png")
+        st.markdown("**Streamlit**")
 
     with col4:
-        st.markdown("### **Visualizaciones**")
-        st.write("**Power BI**")
+        st.markdown("### Visualizaciones")
+        st.image("sources/I_PowerBI.png")
+        st.markdown("**Power BI**")
 
     st.write("\n")
     st.write("\n")
@@ -42,14 +44,7 @@ def mostrar(nasdaq_tickers_historic, nasdaq_tickers_info):
         
     st.write("\n")
     st.write("\n")
-    st.write("1. Extracción y tratamiento de datos:")
-    st.write("2. Creación de base de datos:")
-    st.write("3. Modelado de datos MySQL.")
-    st.write("4. Análisis de datos y visualización.")
-    st.write("5. Desarrollo de Modelos de Machine Learning - Clustering y Clasificación.")
-    st.write("6. Creación de aplicación interactiva.")
-    st.write("7. Integración de Power BI para vista usuario.")
-    
+    st.image("sources/D_F_proyecto.drawio.png")
     st.write("\n")
     st.write("\n")
         
@@ -62,23 +57,23 @@ def mostrar(nasdaq_tickers_historic, nasdaq_tickers_info):
 
     with col1:
         st.markdown("**Elsa Melara**")
-        st.write("[LindedIn](https://www.linkedin.com/in/elsa-m-356055b9) | [GitHub](https://github.com/1991Elsa)")
+        st.write("[LinkedIn](https://www.linkedin.com/in/elsa-m-356055b9) | [GitHub](https://github.com/1991Elsa)")
         st.image("https://github.com/1991Elsa.png", width=100)
         
 
     with col2:    
         st.markdown("**Guadalupe Peña**")
-        st.write("[LindedIn](https://www.linkedin.com/in/guadalupe-peña-egea-3aaa8b329) | [GitHub](https://github.com/AdaXana)")
+        st.write("[LinkedIn](https://www.linkedin.com/in/guadalupe-peña-egea-3aaa8b329) | [GitHub](https://github.com/AdaXana)")
         st.image("https://ca.slack-edge.com/T01LJTV7F8F-U07PHJDQ2UU-1cd26fc26974-72", width=100)
 
     with col3:
         st.markdown("**Marina Dominguez**")
-        st.write("[LindedIn](https://www.linkedin.com/in/marina-dominguez-28639b325) | [GitHub](https://github.com/Marina90d)")
+        st.write("[LinkedIn](https://www.linkedin.com/in/marina-dominguez-28639b325) | [GitHub](https://github.com/Marina90d)")
         st.image("https://ca.slack-edge.com/T01LJTV7F8F-U07PF3P8GSE-aaba0a24d2c9-72", width=100)
 
     with col4:
         st.markdown("**Mikel Alonso**")
-        st.write("[LindedIn](https://www.linkedin.com/in/mikel-alonso-alvarez-187776328) | [GitHub](https://github.com/Wantumaka)")
+        st.write("[LinkedIn](https://www.linkedin.com/in/mikel-alonso-alvarez-187776328) | [GitHub](https://github.com/Wantumaka)")
         st.image("https://media.licdn.com/dms/image/v2/D4E03AQHI8B3GYBXpTA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1726060899129?e=1747267200&v=beta&t=r7sEmfM8nP1vcQz0FUBGXRmu2nfc5dTvTP7x5OHGY9g", width=100)
 
 
